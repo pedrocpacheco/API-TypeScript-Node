5eeb142b2fcb435953f30616d31d464858a93624
pedrocpacheco <cpedropacheco@hotmail.com>
2023-08-06 14:31:38 -0300

    config/test: changing outDir to ./dist

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,11 @@
 import express from "express";
 
-const app = express()
+const app = express();
+                    // ? Nome
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.json( { message: "Hello World!" } );
+});
+
+app.listen(PORT, () => console.log(`Server Running at PORT: ${PORT}`) );
